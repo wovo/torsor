96b46728da3d59277f328400c496eae4aa872c38
Wouter van Ooijen <wouter@voti.nl>
2019-05-29 08:35:53 +0200

    update

--- tests/test-runtime.cpp
+++ tests/test-runtime.cpp
@@ -2,7 +2,7 @@
 //
 // test-runtime.hpp
 //
-// torsor compiler error messages tests
+// torsor runtime tests
 //
 // https://www.github.com/wovo/torsor
 // 
@@ -17,34 +17,88 @@
 #include "torsor.hpp"
 
 
+// ==========================================================================
+//
+// simple test framework
+//
+// ==========================================================================
+
+int tests_total = 0;
+int tests_failed = 0;
+
+template< typename A, typename B >
+void test_ok( 
+   bool ok,                          // Test OK?
+   const char * f, int n,            // file name and line number
+   const char * ta, const char *tb,  // arguments, stringified
+   const A & a, const B & b          // arguments, as-is
+){
+   ++tests_total;	
+   if( ! ok ){
+      ++tests_failed;
+      std::cout 
+         << f << ":" << std::dec << n 
+         << " check failed \n" 
+         << "   HWLIB_TEST_EQUAL( " << ta << " , " << tb << " )\n"
+         << "   left  \"" << ta << "\" = " << a << "\n"
+         << "   right \"" << tb << "\" = " << b << "\n\n";
+   }
+}
+
+#define TEST_EQUAL( a, b ) \
+   test_ok( (a) == (b), __FILE__, __LINE__, #a, #b, a, b );   
+
+#define TEST_NOT_EQUAL( a, b ) \
+   test_ok( (a) != (b), __FILE__, __LINE__, #a, #b, a, b );   
+
+int test_end(){
+   if( tests_failed == 0 ){
+      std::cout 
+	     << "\nRuntime test success: " 
+		 << std::dec << tests_total
+		 << " test(s) were successfull\n";
+      return 0;
+   } else {	   
+      std::cout 
+	     << "\nRUNTIME TEST FAILURE: " 
+		 << std::dec << tests_failed
+		 << " test(s) were NOT successfull\n";
+      return -1;
+   }
+}
+
+
+// ==========================================================================
+//
+// the tests 
+//
+// ==========================================================================
+
 void test_constructor(){
    torsor< int > a;
    torsor< int > b;
-   
-   // created equal
+
+   // constructed equal
    TEST_EQUAL( a, b );
-   
-   // equality is not accidental
    a += 2;
+
+   // and this is not fake, a real value is maintained
    TEST_NOT_EQUAL( a, b );
    b += 2;
    TEST_EQUAL( a, b );
 }
 
-void test_add(){
-   torsor< int > a;
-   torsor< int > b;
-   
-   a += 2;
-   TEST_EQUAL( a, b + 2 );
-   TEST_EQUAL( a, 2 + b );
-   
-   a = 2;
+void test_adition(){
+   torsor< uint8_t > a;
+   torsor< uint16_t > b;
+
+   a += 9;
+   TEST_EQUAL( a, b + 9 );
+//   TEST_EQUAL( a, 2 + b );
 }
 
 int main(){
    test_constructor();
-   test_add();
 
    return test_end();
 }
